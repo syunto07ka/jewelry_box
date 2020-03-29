b7e5370c042b7d766ba265657b799d61c7060990
垣井駿努 <syunto07ka@yahoo.co.jp>
2020-03-29 14:49:07 +0900

    Revert "feat: change to css modules"
    This reverts commit 0530a7c9401d072458edeffb92e41664e249cbb8.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import styles from './App.module.scss';
+import './App.scss';
 
 function App() {
   return (
-    <div className={styles.App}>
-      <header className={styles.AppHeader}>
-        <img src={logo} className={styles.AppLogo} alt="logo" />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className={styles.AppLink}
+          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
