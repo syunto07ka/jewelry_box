72429fae87005a8258257fc004aedc0e12c29184
垣井駿努 <syunto07ka@yahoo.co.jp>
2020-04-02 00:01:52 +0900

    style: change classNema to camel case

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import styles from './App.module.scss';
 
 function App() {
   return (
-    <div className={styles.App}>
-      <header className={styles.AppHeader}>
-        <img src={logo} className={styles.AppLogo} alt="logo" />
+    <div className={styles.app}>
+      <header className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className={styles.AppLink}
+          className={styles.appLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
