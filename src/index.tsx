8983b7f59e825594ac2d1de90e147b89306d8b04
垣井駿努 <syunto07ka@yahoo.co.jp>
2020-04-11 20:59:04 +0900

    feat: exchange App to Pages component

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Pages from './containers/pages';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Pages />,
   document.getElementById('root')
 );
 
