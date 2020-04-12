8130572fc1befb38912ff28277ee9ca8a8f6c4ea
垣井駿努 <syunto07ka@yahoo.co.jp>
2020-04-13 00:56:43 +0900

    feat: design idea

--- src/containers/pages/index.tsx
+++ src/containers/pages/index.tsx
@@ -1,9 +1,40 @@
 import * as React from 'react';
+import styles from './styles.module.scss';
 
 const Pages: React.FC = () => {
   return(
     <div>
-      Pages Component
+      <div className={styles.header}>
+        <div className={styles.heading}>Jewelry Box</div>
+        <div className={styles.subHeading}>Kakki's Portfolio Site</div>
+        <div>
+          <ul className={styles.tabBar}>
+            <li className={styles.tab}>Blog</li>
+            <li className={styles.tab}>Profile</li>
+            <li className={styles.tab}>Other</li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.main}>
+        <div>Contents</div>
+        <div>
+          <ul className={styles.cards}>
+            <li className={styles.card}>
+              <div></div>
+              <div>Blog</div>
+            </li>
+            <li className={styles.card}>
+              <div></div>
+              <div>Profile</div>
+            </li>
+            <li className={styles.card}>
+              <div></div>
+              <div>Other</div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div></div>
     </div>
   );
 }
