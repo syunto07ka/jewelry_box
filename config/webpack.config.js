fdd9f79221db480ebbfacb0c445f3c3991ef1580
垣井駿努 <syunto07ka@yahoo.co.jp>
2020-04-12 20:39:32 +0900

    fix: webpackの設定変更 styleNameとclassNameのcssローカルスコープ処理が競合しないようにlocalIdentNameを設定

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -81,7 +81,12 @@ module.exports = function(webpackEnv) {
       },
       {
         loader: require.resolve('css-loader'),
-        options: cssOptions,
+        options: {
+          ...cssOptions,
+          modules: {
+            localIdentName: "[path]___[name]__[local]___[hash:base64:5]"
+          }
+        },
       },
       {
         // Options for PostCSS as we reference these options twice
