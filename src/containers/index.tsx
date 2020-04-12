0d822639498e37f302f6927133d850baf67e8f2e
垣井駿努 <syunto07ka@yahoo.co.jp>
2020-04-12 22:26:49 +0900

    feat: set route in src/containers/index.tsx

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Pages from './pages';
 
 const Containers: React.FC = () => (
-  <div>Containers Component</div>
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Pages} exact />
+    </Switch>
+  </BrowserRouter>
 );
 
 export default Containers;
