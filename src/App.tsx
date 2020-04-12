4e4b367d91f07777fb68deb765350e711798baeb
垣井駿努 <syunto07ka@yahoo.co.jp>
2020-04-12 20:43:00 +0900

    fix: scssファイル名称変更とApp.tsxをclassName -> styleNameに

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import styles from './App.module.scss';
+import './App.scss';
 
 function App() {
   return (
-    <div className={styles.app}>
-      <header className={styles.appHeader}>
-        <img src={logo} className={styles.appLogo} alt="logo" />
+    <div styleName="app">
+      <header styleName="appHeader">
+        <img src={logo} styleName="appLogo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className={styles.appLink}
+          styleName="appLink"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
