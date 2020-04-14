bc074333af08d4bb334e11be03c0ceafe2a5efcc
垣井駿努 <syunto07ka@yahoo.co.jp>
2020-04-15 03:02:36 +0900

    style: change to one liner

--- src/containers/pages/index.tsx
+++ src/containers/pages/index.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 
-const Pages: React.FC = () => {
-  return(
-    <div>
-      Pages Component
-    </div>
-  );
-}
+const Pages: React.FC = () => (
+  <div>
+    Pages Component
+  </div>
+)
 
 export default Pages;
