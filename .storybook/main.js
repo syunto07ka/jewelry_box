23bf38fd285b21841422d9240f6b60d813bf234e
垣井駿努 <syunto07ka@yahoo.co.jp>
2020-05-14 00:25:33 +0900

    fix: fix the reference path

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
+  stories: ['../src/components/*.stories.js'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
   webpackFinal: async config => {
     // do mutation to the config
